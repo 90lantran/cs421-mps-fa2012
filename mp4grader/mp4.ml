3fe032019758d444cf979a7b045c41bf650872dd
jizhu <zinjiggle@gmail.com>
2012-09-20 13:05:02 -0500

    mp4

---
+++ mp4grader/mp4.ml
@@ -0,0 +1,61 @@
+open Mp4common
+
+(* Problem 1 *)
+let addk i j k =
+  raise(Failure "Function not implemented yet.")
+let subk i j k =
+  raise(Failure "Function not implemented yet.")
+let timesk i j k =
+  raise(Failure "Function not implemented yet.")
+let plusk m n k =
+  raise(Failure "Function not implemented yet.")
+let take_awayk m n k =
+  raise(Failure "Function not implemented yet.")
+let multk m n k =
+  raise(Failure "Function not implemented yet.")
+let catk s t k =
+  raise(Failure "Function not implemented yet.")
+let consk x l k =
+  raise(Failure "Function not implemented yet.")
+let lessk a b k =
+  raise(Failure "Function not implemented yet.")
+let eqk x y k =
+  raise(Failure "Function not implemented yet.")
+
+(* Problem 2 *)
+let abcdk a b c d k =
+  raise(Failure "Function not implemented yet.")
+
+(* Problem 3 *)
+let fact_range n m = 
+  raise(Failure "Function not implemented yet.")
+let fact_rangek n m k = 
+  raise(Failure "Function not implemented yet.")
+
+(* Problem 4 *)
+let duplicate_evens l = 
+  raise(Failure "Function not implemented yet.")
+let duplicate_evensk l k = 
+  raise(Failure "Function not implemented yet.")
+
+(* Problem 5 *)
+let first_or_default n m = 
+  raise(Failure "Function not implemented yet.")
+let first_or_defaultk n m k = 
+  raise(Failure "Function not implemented yet.")
+
+(* Problem 6 *)
+let app_all n m = 
+  raise(Failure "Function not implemented yet.")
+let app_allk n m k = 
+  raise(Failure "Function not implemented yet.")
+
+(* Problem 7 *)
+let sum_wholes l = 
+  raise(Failure "Function not implemented yet.")
+let sum_wholesk l k xk = 
+  raise(Failure "Function not implemented yet.")
+
+(* Problem 8 *)
+let rec average_averagek ll k empty_list_list_xk empty_list_in_list_xk =
+  raise(Failure "Function not implemented yet.")
