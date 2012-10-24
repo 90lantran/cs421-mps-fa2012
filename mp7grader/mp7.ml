8d9f6be31c8d2df49a5eee52949fd0c04a7af152
jizhu <zinjiggle@gmail.com>
2012-10-23 20:40:18 -0500

    mp7

--- mp7grader/mp7.ml
+++ mp7grader/mp7.ml
@@ -2,7 +2,7 @@
  * File: mp7-skeleton.ml
 
 When I code this homework, I carefully read mp6 of fall 2011. Part of the code, though
-not directly copied from mp6, has similar structures and implimentations as mp6 of fall 2011
+not directaily copied from mp6, has similar structures and implimentations as mp6 of fall 2011
 
  Reference:
   http://courses.engr.illinois.edu/cs421/fa2011/mps/MP6/
@@ -24,71 +24,68 @@ let asMonoTy4 () =
 
 (* Problem 2 *)
 let rec subst_fun subst m =
-    match subst with [] -> TyVar m
+    match subst with 
+    | [] -> TyVar m
     | (a,b) :: rest -> if a = m then b else subst_fun rest m
 
 (* Problem 3 *)
 let rec monoTy_lift_subst subst monoTy =
-    match monoTy
-    with TyVar m -> subst_fun subst m
+    match monoTy with 
+    | TyVar m -> subst_fun subst m
     | TyConst(n, typel) -> TyConst(n, List.map (monoTy_lift_subst subst) typel)
 
 (* Problem 4 *)
 let rec occurs x ty =
-    match ty with TyVar m -> x = m
+    match ty with 
+    | TyVar m -> x = m
     | TyConst(n, typel) -> List.exists (occurs x) typel
 
 (* Problem 5 *)
 let rec unify eqlst : substitution option =
-  let rec newAdd l1 l2 m =
-    match l1,l2 with [],[] -> Some m
-    | h::tail, h'::tail' -> newAdd tail tail' ((h,h')::m)
-    | _ -> None
+  let rec newAdd l1 l2 m = match l1,l2 with 
+      | [],[] -> Some m
+      | h::tail, h'::tail' -> newAdd tail tail' ((h,h')::m)
+      | _ -> None
   in
-  match eqlst with [] -> Some([])
+  match eqlst with 
+  | [] -> Some([])
   | (s,t)::resteq ->
     if s = t then unify resteq
     else (match (s,t) 
-          with (TyConst(c, tl), TyConst(c', tl')) ->
-            if c=c' then (match (newAdd tl tl' resteq) with None -> None | Some l -> unify l)
-            else None
-          | (TyConst(c, tl), TyVar(m)) -> unify ((TyVar(m), TyConst(c, tl))::resteq)
-          | (TyVar(n),t) ->
-             if (occurs n t)
-             then None
-             else let resteq' =
-                      List.map
-                      (fun (t1,t2) ->
-                           (monoTy_lift_subst [(n,t)] t1, monoTy_lift_subst [(n,t)] t2))
-                      resteq
-                   in (match unify resteq'
-                       with None -> None
-                       | Some(phi) -> Some((n, monoTy_lift_subst phi t):: phi)))
+          with 
+          | (TyVar(n),t) -> if (occurs n t) then None
+             else let newresteq = List.map
+                (fun (x1,x2) -> 
+                  (monoTy_lift_subst [(n,t)] x1, monoTy_lift_subst [(n,t)] x2)) 
+                resteq in 
+                (match unify newresteq with None -> None
+                      | Some(p) -> Some((n, monoTy_lift_subst p t):: p)))
+          | (TyConst(h, tail), TyConst(h', tail')) -> 
+              if h=h' then 
+                (match (newAdd tail tail' resteq) with 
+                      | None -> None 
+                      | Some x -> unify x)
+              else None
+          | (TyConst(h, tail), TyVar(m)) -> 
+              unify ((TyVar(m), TyConst(h, tail))::resteq)
 
 (* Problem 6 *)
 
-let rec first p l =
-    match l with [] -> None
-    | (x :: xs) -> if p x then Some x else first p xs
-
-let canonicalize ty =
-let rec canon_type subst m ty =
-   match ty
-   with TyVar n ->
-     (match first (fun p -> fst p = n) subst
-      with Some (j,k) -> (subst, m, TyVar k)
-      | None -> ((n,m)::subst), m+1, TyVar m)
+let helpcanon inputty =
+  let rec findfirst p l = match l with [] -> None
+    | (x :: xs) -> if p x then Some x else findfirst p xs in
+let rec rec_canon su m ty =
+   match ty with 
+   | TyVar n -> (match findfirst (fun p -> fst p = n) su with 
+            | Some (l1,l2) -> (su, m, TyVar k)
+            | None -> ((n,m)::su), m+1, TyVar m)
    | TyConst (c, tys) ->
-     (match
-       List.fold_left
-       (fun (subst, n, tyl) -> fun ty ->
-        (match canon_type subst n ty
-         with (subst', n', ty') -> (subst', n', ty'::tyl)))
-       (subst, m, [])
-       tys
-      with (new_subst, new_m, new_tys) -> (new_subst, new_m, TyConst(c, List.rev new_tys)))
-  in let (_, _, c_ty) = canon_type [] 0 ty in c_ty
+     (match List.fold_left (fun (su, n, tyl) -> fun ty -> 
+      (match rec_canon su n ty with (nsu, nn, nty) -> (nsu, nn, nty::tyl)))
+       (su, m, []) tys
+      with  (n_su, n_m, n_tys) -> (n_su, n_m, TyConst(c, List.rev n_tys)))
+  in let (_, _, tty) = rec_canon [] 0 inputty in tty
 
 let equiv_types ty1 ty2 =
-   let new_ty1 = canonicalize ty1 in
-   let new_ty2 = canonicalize ty2 in new_ty1 = new_ty2;;
+   let ty1' = helpcanon ty1 in
+   let ty2' = helpcanon ty2 in ty1' = 'ty2;;
